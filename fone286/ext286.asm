fcc4065ed444bfd99b6694548a534a74b368a894
fone32 <fone32@users.noreply.github.com>
2014-08-24 00:22:23 +0200

    Added chnages to handle error from protected mode after DPMI switch takes place.

--- fone286/ext286.asm
+++ fone286/ext286.asm
@@ -13,17 +13,26 @@ init_extender:
 	call _extender
 	jnc @f
 	pop bx
-	call ..critical_error
+	call ..extender_critical_error
 .exterror_msg	 db 13,10, ' f/One286 DOS Extender Runtime failed to run.',13,10,'$'
 
 @@:
 	xor ax,ax
 	ret
 
-..critical_error:
+..extender_critical_error:
+	push ax
 	pop dx
 	push bx
 	mov ah,9
 	int 21h
+	pop ax
+	cmp ax,6	;return code below 6 are from real mode
+	jae @f
+	stc
 	ret
+@@:			;since we failed after switch to PM we end up
+			;according to DPMI spec with Int 21h 4c
+	mov ah,4ch
+	int 21h
 
